c635d9530cf6765c242bd1e0288aaa74aa0e6229
Dominik <dominik.jojczyk@gmail.com>
2025-01-30 18:03:02 +0100

    1.2

--- components/reviews/ReviewItem.jsx
+++ components/reviews/ReviewItem.jsx
@@ -1,11 +1,11 @@
 import { IoStar } from "react-icons/io5";
 
-export default function ReviewItem() {
+export default function ReviewItem(props) {
   return (
     <div className="p-6 bg-white rounded-xl">
       <div className="flex justify-between gap-3">
         <div>
-          <h3 className="font-bold text-lg">Anna Kowalska</h3>
+          <h3 className="font-bold text-lg">{props.name}</h3>
         </div>
         <div className="flex">
           <IoStar className="text-2xl text-[#EFB036]" />
@@ -15,10 +15,7 @@ export default function ReviewItem() {
           <IoStar className="text-2xl text-[#EFB036]" />
         </div>
       </div>
-      <p className="mt-8">
-        Współpraca z Patrykiem to czysta przyjemność. Stworzył dla mnie logo,
-        które idealnie oddaje charakter mojej marki. Zdecydowanie polecam!
-      </p>
+      <p className="mt-8">{props.text}</p>
     </div>
   );
 }
--- components/reviews/Reviews.jsx
+++ components/reviews/Reviews.jsx
@@ -1,11 +1,24 @@
 import ReviewItem from "./ReviewItem";
 export default function Reviews() {
   return (
-    <section className="px-6 min-h-[100vh] bg-black ">
+    <section className="px-6 min-h-[100vh] bg-black  ">
       <h2 className="uppercase text-3xl leading-snug py-10 text-white mb-16">
         Sprawdź opinie o mojej pracy
       </h2>
-      <ReviewItem />
+      <div className="grid gap-8 pb-16">
+        <ReviewItem
+          name="Anna Kaszuba"
+          text="Współpraca z [Imię Grafika] to czysta przyjemność. Stworzył dla mnie logo, które idealnie oddaje charakter mojej marki. Zdecydowanie polecam!"
+        />
+        <ReviewItem
+          name="Tomasz Wąsik"
+          text="Projekt przekroczył moje oczekiwania! Identyfikacja wizualna, którą otrzymałem, jest spójna, nowoczesna i doskonale pasuje do mojej firmy."
+        />
+        <ReviewItem
+          name="Katarzyna Małek"
+          text="Nie tylko świetny design, ale też doskonałe zrozumienie moich potrzeb. Współpraca przebiegła sprawnie, a efekty są rewelacyjne!"
+        />
+      </div>
     </section>
   );
 }
