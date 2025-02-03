128cd216aff1e85c74c00a84fb5e6e19e660b6dc
Dominik <dominik.jojczyk@gmail.com>
2025-02-03 19:44:09 +0100

    Update portfolioSlug.js

--- app/portfolio/[slug]/portfolioSlug.js
+++ app/portfolio/[slug]/portfolioSlug.js
@@ -44,7 +44,7 @@ export default function PortfolioSlug({ project }) {
   return (
     <>
       <Nav />
-      <div className="px-6 md:px-16 lg:px-20 xl:px-32 md:py-20 xl:py-24">
+      <div className="px-6  md:px-16 lg:px-20 xl:px-32 pt-10">
         <div className="flex flex-col lg:flex-row lg:gap-24 justify-between">
           <div className="lg:w-3/5">
             <h1 className="text-4xl md:text-5xl">{project.title}</h1>
@@ -65,7 +65,7 @@ export default function PortfolioSlug({ project }) {
         </div>
 
         {project.additionalImages.length > 0 && (
-          <div className="flex flex-col md:flex-row gap-8 mt-16 lg:mt-24 xl:mt-44 pb-20 md:pb-0">
+          <div className="flex flex-col md:flex-row gap-8 mt-16 lg:mt-24 xl:mt-44 pb-20">
             {project.additionalImages.map((image, index) => (
               <div className="xl:w-1/3" key={index}>
                 <Image
