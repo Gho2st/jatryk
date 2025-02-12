fc238969584dc6e135ddd6a8c09c828496f2170e
Dominik <dominik.jojczyk@gmail.com>
2025-02-12 21:44:27 +0100

    Update page.js

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -1,13 +1,13 @@
 import ContactPage from "./contact";
 
 export const metadata = {
-  title: "",
+  title: "Kontakt | Jatryk - Profesjonalna Grafik | Patryk Jędrzejek",
   alternates: {
-    canonical: "/slug",
+    canonical: "/kontakt",
   },
 
   description:
-  "tu opis"
+    "Skontaktuj się z Jatryk! Profesjonalne usługi graficzne, projektowanie logo, branding i grafika reklamowa. Sprawdź moje portfolio i napisz do mnie!",
 };
 
 export default function Page() {
