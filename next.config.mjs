56244a9b1706f72a5d956c39a58002bfe7b80425
Dominik <dominik.jojczyk@gmail.com>
2025-03-31 17:41:39 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"], // Dodaj tutaj tę linię, aby pozwolić na ładowanie obrazów z Firebase
+    unoptimized: true,
   },
 };
 
